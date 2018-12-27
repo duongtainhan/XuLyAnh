69ccffe0271202fcc1fddb74c31fd85b695025f7
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-12-27 20:16:50 +0700

    Update version 1.2

--- app/src/main/java/library/ict/xulyanh/MainActivity.java
+++ app/src/main/java/library/ict/xulyanh/MainActivity.java
@@ -1,10 +1,8 @@
 package library.ict.xulyanh;
 
-import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import library.ict.xuly.XuLyAnh;
+import library.ict.xulyanh.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Bitmap bitmap = null;
-        XuLyAnh.doHighlightImage(bitmap);
     }
 }
